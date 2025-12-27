0be396883d3a6957019a248c2e76eb47dc707295
Yuhan Wang <1048429491@qq.com>
2025-12-27 21:00:59 +0800

    fix: gvm driver update for threadIdx

--- driver/gvm_device/ventus.cpp
+++ driver/gvm_device/ventus.cpp
@@ -4,11 +4,12 @@
  *
  * 1. `/include/ventus.h`中声明的函数
  */
+ #define ENABLE_GVM
 
 #include <utils.hpp>
 #include "ventus.h"
 #include "loadelf.hpp"
-#include "ventus_gvm.h"
+#include "ventus_rtlsim.h"
 #include <cstdint>
 #include <cstdlib>
 #include <memory>
@@ -226,17 +227,28 @@ extern int vt_start(vt_device_h hdevice, vt_kernel_metadata_t *mtd_driver, uint6
         .sgprUsage = mtd_driver->sgprUsage,
         .vgprUsage = mtd_driver->vgprUsage,
         .pdsBaseAddr = mtd_driver->pdsBaseAddr,
+        .num_thread_global =
+            {mtd_driver->num_thread_global[0], mtd_driver->num_thread_global[1],
+             mtd_driver->num_thread_global[2]},
+        .num_thread_local =
+            {mtd_driver->num_thread_local[0], mtd_driver->num_thread_local[1],
+             mtd_driver->num_thread_local[2]},
+        .threadIdxOffset =
+            {mtd_driver->threadIdxOffset[0], mtd_driver->threadIdxOffset[1],
+             mtd_driver->threadIdxOffset[2]},
         .num_buffer = 0,
         .buffer_base = nullptr,
         .buffer_size = nullptr,
         .buffer_allocsize = nullptr,
     };
-    logger->debug(
-        "kernel metadata: kernel_id={}, kernel_size=[{}, {}, {}], wf_size={}, wg_size={}, "
-        "metaDataBaseAddr={:x}, ldsSize={}, pdsSize={}, sgprUsage={}, vgprUsage={}, "
-        "pdsBaseAddr={:x}",
+    SPDLOG_LOGGER_DEBUG(
+        logger,
+        "kernel metadata: kernel_id={}, kernel_size=[{}, {}, {}], wf_size={}, "
+        "wg_size=[{}, {}, {}]={}wf, metaDataBaseAddr=0x{:x}, ldsSize=0x{:x}, pdsSize=0x{:x}, "
+        "sgprUsage={}, vgprUsage={}, pdsBaseAddr=0x{:x}",
         mtd_driver->kernel_id, mtd_driver->kernel_size[0], mtd_driver->kernel_size[1],
-        mtd_driver->kernel_size[2], mtd_driver->wf_size, mtd_driver->wg_size,
+        mtd_driver->kernel_size[2], mtd_driver->wf_size, mtd_driver->num_thread_local[0],
+        mtd_driver->num_thread_local[1], mtd_driver->num_thread_local[2], mtd_driver->wg_size,
         mtd_driver->metaDataBaseAddr, mtd_driver->ldsSize, mtd_driver->pdsSize,
         mtd_driver->sgprUsage, mtd_driver->vgprUsage, mtd_driver->pdsBaseAddr
     );
